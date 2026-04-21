3e0a7e289b85077e66e3aac9929940263a93d263
Aathif Mahir <aathifmahir@gmail.com>
2026-04-21 06:56:07 +0530

    Refactor Blazor sample app structure and add project references for improved organization and functionality

--- sample/Circuids.Bridge.Blazor.WebAssembly.Sample/Program.cs
+++ sample/Circuids.Bridge.Blazor.WebAssembly.Sample/Program.cs
@@ -1,4 +1,6 @@
+using Circuids.Bridge.Blazor;
 using Circuids.Bridge.Blazor.WebAssembly.Sample;
+using Circuids.Bridge.Shared.Sample;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +9,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBridgeForBlazor();
+builder.Services.AddBridgeSharedSample();
 
 await builder.Build().RunAsync();
